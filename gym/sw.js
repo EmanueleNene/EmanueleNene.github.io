a1dcc60e49bbe767441ac52980de965f9685c6e2
Emanuele D. <emadal@kth.se>
2026-09-13 19:32:52 +0200

    Allow editing logged workouts and decouple session stats from set ticks

--- gym/sw.js
+++ gym/sw.js
@@ -1,5 +1,5 @@
 /* Bump CACHE whenever you edit index.html, otherwise phones keep the old copy. */
-const CACHE = 'ferrodastiro-v9';
+const CACHE = 'ferrodastiro-v10';
 const ASSETS = [
   './',
   'index.html',
