569fa200c4cacdc208dc6e227cdb96fdb8ec4711
Emanuele D. <emadal@kth.se>
2026-09-14 22:17:02 +0200

    Implement Epley + Brzycki 1RM formula with heavy-set prioritization in Progress tab

--- gym/sw.js
+++ gym/sw.js
@@ -1,5 +1,5 @@
 /* Bump CACHE whenever you edit index.html, otherwise phones keep the old copy. */
-const CACHE = 'ferrodastiro-v14';
+const CACHE = 'ferrodastiro-v15';
 const ASSETS = [
   './',
   'index.html',
