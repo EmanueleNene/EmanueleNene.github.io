453b33d9b25e66d8454614112c98a1503ab7a455
Emanuele D. <emadal@kth.se>
2026-09-16 21:42:03 +0200

    Add Hyperextension (back) and Hyperextensions (thighs) exercises and diagram SVGs

--- gym/sw.js
+++ gym/sw.js
@@ -1,5 +1,5 @@
 /* Bump CACHE whenever you edit index.html, otherwise phones keep the old copy. */
-const CACHE = 'ferrodastiro-v17';
+const CACHE = 'ferrodastiro-v18';
 const ASSETS = [
   './',
   'index.html',
