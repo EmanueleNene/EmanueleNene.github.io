e87353ffc93abe46b433bf32b3fb58123bc95012
Emanuele D. <emadal@kth.se>
2026-09-20 23:00:29 +0200

    fix(gym): elevate rest timer position to clear bottom navigation menu

--- gym/sw.js
+++ gym/sw.js
@@ -1,5 +1,5 @@
 /* Bump CACHE whenever you edit index.html, otherwise phones keep the old copy. */
-const CACHE = 'ferrodastiro-v27';
+const CACHE = 'ferrodastiro-v28';
 const ASSETS = [
   './',
   'index.html',
